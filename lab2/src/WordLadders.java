39886ea2b1de861ce95ee4effb0cea2b82060647
piotr <piotr.czernicki@gmail.com>
2012-03-27 16:47:23 +0200

    first

---
+++ lab2/src/WordLadders.java
@@ -0,0 +1,12 @@
+
+public class WordLadders {
+	String[] words = {"there", "which", "their", "about", "these", "words", "would", "other", "write", "could"};
+	
+	
+	public static void main(String [] args){
+		buildGraph();
+	}
+	
+	private void buildGraph(){};
+	private void runTest(){};
+}
